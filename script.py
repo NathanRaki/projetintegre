6a13d83eb8fc9e1d51fc49c5d0433f61f972af95
ncRakl <nathan.raklure@gmail.com>
2020-11-04 11:50:58 +0100

    affinage du traitement

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ Created on Wed Oct 21 19:58:51 2020
 import pandas as pd
 import nltk
 import re
+import pickle
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 from collections import defaultdict
@@ -32,24 +33,42 @@ for k,v in titles.items() :
     lowered_titles[k] = v.lower()
     
 #%% Cleaning titles
+cleaned_titles = defaultdict(set)
 for k,v in lowered_titles.items() :
-    lowered_titles[k]=re.sub('\[.*?\]','',lowered_titles[k]) # suppression de toutes les expressions entre crochets
-    lowered_titles[k]=re.sub('\{.*?\}','',lowered_titles[k]) # suppression de toutes les expressions entre accolades
-    lowered_titles[k]=re.sub('\(.*?\)','',lowered_titles[k]) # suppression de toutes les expressions entre parenthèses
-    lowered_titles[k]=re.sub('<.+>','',lowered_titles[k])    # suppression de toute la chaine qui se trouve entre un '<' et un '>' (1ère et dernière occurence)
-    lowered_titles[k]=re.sub('\$.+\$','',lowered_titles[k])  # suppression de toute la chaine qui se trouve entre un '$' et un '$' (1ère et dernière occurence)
-    lowered_titles[k]=re.sub('\S*[é,è,\^,_,&,:,@,#,$,\\\]\S*',' ',lowered_titles[k]) # suppression de tous les mots contenant un @ et #.. (@ et # seule inclus aussi)
-    lowered_titles[k]=re.sub('[.,\,,!,?,%,*,§,²,~,\{,\}]',' ',lowered_titles[k]) # suppression des caractères spéciaux
-    lowered_titles[k]=re.sub('[;,+,/]',' ',lowered_titles[k])
-    lowered_titles[k]=re.sub(' +', ' ', lowered_titles[k]) # suppression des espaces en trop
-    lowered_titles[k]=re.sub('-',' ',lowered_titles[k]) # remplacement des tirets par rien # choix qui se discute
-    lowered_titles[k]=re.sub('(\s\d+\s|^\d+\s|\s\d+$)','',lowered_titles[k]) # suppression de tous les chiffres qui sont seuls
-    lowered_titles[k]=re.sub('(\s(.)\s)|(^.\s)|(\s.$)','',lowered_titles[k]) #suppression des caractères d'une lettre
-    lowered_titles[k]=re.sub('(\'s)','',lowered_titles[k]) # suppression des 's
-    lowered_titles[k]=re.sub('\S*((january)|(february)|(march)|(april)|(may)|(june)|(july)|(august)|(september)|(october)|(november)|(december))\S*','',lowered_titles[k]) # suppression des expressions qui continnent un mois
+    cleaned_titles[k] = lowered_titles[k]
+    cleaned_titles[k] = re.sub('\[.*?\]', '', cleaned_titles[k]) # suppression de toutes les expressions entre crochets
+    cleaned_titles[k] = re.sub('\{.*?\}', '', cleaned_titles[k]) # suppression de toutes les expressions entre accolades
+    cleaned_titles[k] = re.sub('\(.*?\)', '', cleaned_titles[k]) # suppression de toutes les expressions entre parenthèses
+    cleaned_titles[k] = re.sub('<.+>', '', cleaned_titles[k])    # suppression de toute la chaine qui se trouve entre un '<' et un '>' (1ère et dernière occurence)
+    cleaned_titles[k] = re.sub('\$.+\$', '', cleaned_titles[k])  # suppression de toute la chaine qui se trouve entre un '$' et un '$' (1ère et dernière occurence)
+    cleaned_titles[k] = re.sub('\S*[é,è,\^,_,&,:,@,#,$,\\\]\S*', ' ', cleaned_titles[k]) # suppression de tous les mots contenant un @ et #.. (@ et # seule inclus aussi)
+    cleaned_titles[k] = re.sub('[.,\,,!,?,%,*,§,²,~,\{,\}]', ' ', cleaned_titles[k]) # suppression des caractères spéciaux
+    cleaned_titles[k] = re.sub('[;,+,/]', ' ', cleaned_titles[k])
+    cleaned_titles[k] = re.sub(' +', ' ', cleaned_titles[k]) # suppression des espaces en trop
+    cleaned_titles[k] = re.sub('-', ' ', cleaned_titles[k]) # remplacement des tirets par rien # choix qui se discute
+    cleaned_titles[k] = re.sub('(\s\d+\s|^\d+\s|\s\d+$)', '', cleaned_titles[k]) # suppression de tous les chiffres qui sont seuls
+    cleaned_titles[k] = re.sub('(\s(.)\s)|(^.\s)|(\s.$)', '', cleaned_titles[k]) #suppression des caractères d'une lettre
+    cleaned_titles[k] = re.sub('(\'s)', '', cleaned_titles[k]) # suppression des 's
+    cleaned_titles[k] = re.sub('\S*((january)|(february)|(march)|(april)|(may)|(june)|(july)|(august)|(september)|(october)|(november)|(december))\S*', '', cleaned_titles[k]) # suppression des expressions qui continnent un mois
+    cleaned_titles[k] = re.sub('([0-9]+)(?:st|nd|rd|th)?', '', cleaned_titles[k]) # supprime les nombres ainsi que les 2nd, 14th, 21st, ...
+    cleaned_titles[k] = re.sub('[*µ*]', '', cleaned_titles[k]) # supprime tout ce qui contient µ
+    if (len(cleaned_titles[k]) == 0) or (len(cleaned_titles[k]) == 1) :
+        del cleaned_titles[k]
+
+#%%
+en_ratio = 0.5
+count = 0
+from langdetect import detect_langs
+for k,v in lowered_titles.items() :
+    print('v: ', v, '\n')
+    res = detect_langs(v)
+    for lang in res :
+        if lang.lang == 'en' and lang.prob >= en_ratio :
+            count = count + 1
+print(count)
 
 #%% Tokenizing Titles
-tokenizer = nltk.RegexpTokenizer(r'\w+')
+tokenizer = nltk.RegexpTokenizer(r'\w+') # Création du tokenizer, \w+ supprime les mots vides (sinon nltk.pos_tag() a une erreur)
 tokenized_titles = defaultdict(set)
 for k,v in lowered_titles.items() :
     tokenized_titles[k] = tokenizer.tokenize(v)
@@ -83,10 +102,26 @@ for k,v in optimized_titles.items() :
 for k,v in lemmatized_titles.items() :
     matching = [s for s in v if 'multitask' in s]
     if matching :
-        print(matching)
+        print(v)
+        
+#%% Tests
+maximum = 0
+for k,v in lemmatized_titles.items() :
+    if len(v) > maximum :
+        print(len(v), ' : ', v, '\n')
+        maximum = len(v)
 
 #%% Print Area
 rows_number = 10
 print_dict = dict(list(lemmatized_titles.items())[0:rows_number-1])
 for k,v in print_dict.items() :
     print(k, ':', v)
+    
+# Problèmes :
+## Symboles non-anglophones : Allemand, Français, Langues de l'Est, symbole µ (minoritaire)
+## Les titres trèèès longs (max 36 tokens) peuvent plus influencer les tendances que d'autres
+## Il y a des listes de token vides
+
+# Corrigé :
+## Il reste des termes comme 12th, 11th, 1st, 2nd, 30th, ...
+## Il reste des 100GB, 000, 24fps, et toute sorte de termes pleins de chiffres
